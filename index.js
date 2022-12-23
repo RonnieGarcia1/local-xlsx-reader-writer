319107749d21c51b5c753b5609ee77c4a7983a71
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-12-23 14:57:39 -0500

    added index.js with instructions

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("welcome to my local xlsx reader / writer")
+console.log("read xlsx: $ readsheet.js")
+console.log("write new sheet: $ writeSheet.js")
+console.log("created by Ronnie Garcia")
